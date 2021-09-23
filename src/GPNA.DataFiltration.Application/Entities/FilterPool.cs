2d87eeb77895b73f76b9b06e7b1b4d584927d0ae
peterkrokhin <pkrokhin@gmail.com>
2021-09-23 10:51:48 +0500

    feat: added initial project files

---
+++ src/GPNA.DataFiltration.Application/Entities/FilterPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GPNA.DataFiltration.App
+{
+    public class FilterPool
+    {
+        public long Id { get; set; }
+        public string SourceTopic { get; set; }
+        public string GoodTopic { get; set; }
+        public string BadTopic { get; set; }
+        public Guid AccountId { get; set; }
+        public bool IsEnabled { get; set; }
+        public IEnumerable<FilterConfig> FilterConfigs { get; set; }
+    }
+}
---
+++ src/GPNA.DataFiltration.Application/Entities/ParameterValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPNA.DataFiltration.App.Entities
+{
+    public class ParameterValue
+    {
+        /// <summary>
+        /// Идентификатор скважины OIS (WELL_ID)
+        /// </summary>
+        public long WellId { get; set; }
+
+        /// <summary>
+        /// Внешний идентификатор параметра (PARAM_ID)
+        /// </summary>
+        public int ParameterId { get; set; }
+
+        /// <summary>
+        /// Значение технологического параметра (MEASURE_VALUE)
+        /// </summary>
+        public double? Value { get; set; }
+
+        /// <summary>
+        /// Дата изменения технологического параметра, зафиксированная контроллером (MEASURE_DATE)
+        /// </summary>
+        public DateTime? Timestamp { get; set; }
+
+        /// <summary>
+        /// Периодичность ведения замеров (MEASURE_DURATION)
+        /// </summary>
+        public int Period { get; set; }
+        /// <summary>
+        /// Тип скважины: нагнетательная/нефтяная/газовая
+        /// </summary>
+        //public ObjectType WellType { get; set; }
+    }
+}
